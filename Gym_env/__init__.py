f798202b0b24fa468f096d590a76c97f674ee3b8
adigup390 <65325056+adigup390@users.noreply.github.com>
2021-09-05 15:10:41 +0530

    Update __init__.py

--- Gym_env/__init__.py
+++ Gym_env/__init__.py
@@ -5,8 +5,8 @@ logger = logging.getLogger(__name__)
 
 register(
     id='Env-v0',
-    entry_point='Gym_env.envs:Env1',
-    timestep_limit=1000,
+    entry_point='Gym_env.envs:twoarmedbandit',
+    timestep_limit=1,
     reward_threshold=1.0,
     nondeterministic = True,
 )
