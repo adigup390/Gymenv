6f623c3a53b2c76ef5f8d1280453767b4ab3fb4a
adigup390 <65325056+adigup390@users.noreply.github.com>
2021-09-05 14:58:44 +0530

    Update and rename __init__.py to Gym_env/__init__.py

--- Gym_env/__init__.py
+++ Gym_env/__init__.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 register(
     id='Env-v0',
-    entry_point='Gymenv.envs:Env1',
+    entry_point='Gym_env.envs:Env1',
     timestep_limit=1000,
     reward_threshold=1.0,
     nondeterministic = True,
